daeef23c637febe9dfb52255384ef790ca27278a
anx-ctrink <72492995+anx-ctrink@users.noreply.github.com>
2020-10-07 12:15:34 +0200

    Update myfile.py
    try binary example  from manual

--- myfile.py
+++ myfile.py
@@ -1,4 +1,13 @@
+import base64
+ 
 def myfunction(event, context):
+    input_binary = base64.b64decode(event['data']['binary'])
+    output_binary = bytes(map(lambda b: b ^ 255, input_binary))
     return {
-        'data': event['data']['a'] + event['data']['b'],
+        'type': 'binary',
+        'data': {
+            'binary': base64.b64encode(output_binary).decode('ascii'),
+            'name': 'output.blob',
+            'content_type': 'x-my-first-function/blob',
+        },
     }
