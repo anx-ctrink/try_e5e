da5f7824e7bddba5c9311ec0fb1d8fc2e0e7a3dd
anx-ctrink <72492995+anx-ctrink@users.noreply.github.com>
2020-10-07 12:33:09 +0200

    Update myfile.py
    example from manual with HTTP header

--- myfile.py
+++ myfile.py
@@ -1,13 +1,7 @@
-import base64
- 
 def myfunction(event, context):
-    input_binary = base64.b64decode(event['data']['binary'])
-    output_binary = bytes(map(lambda b: b ^ 255, input_binary))
     return {
-        'type': 'binary',
-        'data': {
-            'binary': base64.b64encode(output_binary).decode('ascii'),
-            'name': 'output.blob',
-            'content_type': 'x-my-first-function/blob',
+        'response_headers': {
+            'x-sent-content-type': event['request_headers'].get('content-type', '*/*'),
         },
+        'data': 'Hello world!',
     }
