ea6fff2adefdaddb01cc0bf848d0dc3e26fd8261
anx-ctrink <72492995+anx-ctrink@users.noreply.github.com>
2020-10-12 07:59:09 +0200

    Update myfile.py
    simple hello world

--- myfile.py
+++ myfile.py
@@ -1,7 +1,4 @@
 def myfunction(event, context):
     return {
-        'response_headers': {
-            'x-sent-content-type': event['request_headers'].get('content-type', '*/*'),
-        },
         'data': 'Hello world!',
     }
